4700a127460599cc1da85967bf38b9c3e4a8d0b1
khoa le <kevinle38@gmail.com>
2022-04-28 10:38:03 +0700

    done

--- excel.py
+++ excel.py
@@ -31,15 +31,13 @@ def main():
                 file_path = os.path.join(root, file)
                 with open(file_path, "r") as js:
                     data = json.load(js)
-                    data["fileName"] = file_path.replace(
-                        str(result_path), "").replace("json", "kp")
                     appendToCsv(data)
 
     i = 1
     df = pandas.DataFrame.from_dict(
         {'name': csv["name"], 'amountOfItem': csv["amountOfItem"], 'capacities': csv["capacities"],'totalWeight':csv["totalWeight"]
         ,'runTime':csv["runTime"],'Optimal':csv["Optimal"]})
-    df.to_excel('test.xlsx', header=True, index=False)
+    df.to_excel('test1.xlsx', header=True, index=False)
 
 
 main()
